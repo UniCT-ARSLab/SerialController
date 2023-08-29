cffbec89f4f36fa5003d4d30b738837de8df50a3
Teosche <matteojacopo.schembri@gmail.com>
2023-08-29 19:50:00 +0200

    fix: error check

--- server/main.go
+++ server/main.go
@@ -1,9 +1,18 @@
 package main
 
-import "github.com/gin-gonic/gin"
+import (
+	"fmt"
+
+	"github.com/gin-gonic/gin"
+)
 
 func main() {
 	r := gin.Default()
 
-	r.Run() // listen and serve on 0.0.0.0:8080
+	err := r.Run() // listen and serve on 0.0.0.0:8080
+
+	if err != nil {
+		fmt.Println("An error occurred:", err)
+		return
+	}
 }
