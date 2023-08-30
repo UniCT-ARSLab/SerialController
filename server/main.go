71f143c3269e1b28376b9fbc263a8b620e82deb2
Helias <stefanoborzi32@gmail.com>
2023-08-30 13:42:10 +0200

    feat: add end-point /ping

--- server/main.go
+++ server/main.go
@@ -2,12 +2,19 @@ package main
 
 import (
 	"fmt"
+	"net/http"
 
 	"github.com/gin-gonic/gin"
 )
 
 func main() {
 	r := gin.Default()
+
+	r.GET("/ping", func(c *gin.Context) {
+    c.JSON(http.StatusOK, gin.H{
+      "message": "pong",
+    })
+  })
 
 	err := r.Run() // listen and serve on 0.0.0.0:8080
 
